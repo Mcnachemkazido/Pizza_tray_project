8e561afd68ab175388643f07a4e0674c68845bc0
MenachemKazido <agbg7172@gmail.com>
2026-02-16 19:36:15 +0200

    finish the text worker

--- text_worker/consumer.py
+++ text_worker/consumer.py
@@ -1,16 +1,16 @@
 from dotenv import load_dotenv
 import os
 import json
-from time import sleep
 from confluent_kafka import Consumer
 from mongo import mongo_coll
+import utils
 load_dotenv()
 
 
 
 consumer_config = {
     "bootstrap.servers": os.getenv("PIZZA_HOST_PORT_KAFKA"),
-    "group.id": "kitchen-team",
+    "group.id": "text-team",
     "auto.offset.reset": "earliest"
 }
 
@@ -27,11 +27,16 @@ try:
             continue
         data = msg.value().decode('utf-8')
         pizza = json.loads(data)
-        query_filter = {'unique_id':pizza['unique_id']}
-        update_operation = {'$set': {'status':'DELIVERED'}}
-        mongo_coll.update_one(query_filter,update_operation)
-        print(f"🤩🤩🤩 I updated the Status field in the object: {pizza['unique_id']}")
-        sleep(2)
+
+        data_searching = utils.search_in_text(pizza)
+        data_clearing = utils.clearing_the_data(data_searching)
+        utils.clearing_the_data(pizza)
+        query_filter = {'unique_id': pizza['unique_id']}
+        update_operation = {'$set': {"cleaned_protocol":data_clearing['cleaned_protocol'],
+                                     "allergies_flagged":data_clearing['allergies_flagged']}}
+        mongo_coll.update_one(query_filter, update_operation)
+        print(f"😃😃😃😃😃I updated two additional fields in mongodb in user\n{pizza['unique_id']}")
+
 except KeyboardInterrupt:
     print("good by 👋👋👋")
 
