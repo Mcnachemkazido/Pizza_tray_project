84c070fabd9c4dc4f2f67158688300a030b2ba59
MenachemKazido <agbg7172@gmail.com>
2026-02-16 15:40:57 +0200

    finish kitchen_worker

--- kitchen_worker/consumer.py
+++ kitchen_worker/consumer.py
@@ -1,7 +1,7 @@
-from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 import json
+from time import sleep
 from confluent_kafka import Consumer
 from mongo import mongo_coll
 load_dotenv()
@@ -17,17 +17,24 @@ consumer_config = {
 consumer = Consumer(consumer_config)
 consumer.subscribe(["orders-pizza"])
 
-while True:
-    msg = consumer.poll(1.0)
-    if msg is None:
-        continue
-    if msg.error():
-        print(f"☢️☢️☢️ {msg.error()}")
-        continue
-    data = msg.value().decode('utf-8')
-    pizza = json.loads(data)
-    query_filter = {'unique_id':pizza['unique_id']}
-    update_operation = {'$set': {' status': 'DELIVERED'}}
+try:
+    while True:
+        msg = consumer.poll(1.0)
+        if msg is None:
+            continue
+        if msg.error():
+            print(f"☢️☢️☢️ {msg.error()}")
+            continue
+        data = msg.value().decode('utf-8')
+        pizza = json.loads(data)
+        query_filter = {'unique_id':pizza['unique_id']}
+        update_operation = {'$set': {'status':'DELIVERED'}}
+        mongo_coll.update_one(query_filter,update_operation)
+        print(f"🤩🤩🤩 I updated the Status field in the object: {pizza['unique_id']}")
+        sleep(2)
+except KeyboardInterrupt:
+    print("good by 👋👋👋")
+
 
 
 
